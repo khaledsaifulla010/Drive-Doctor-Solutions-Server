69235da8339261114a4f43e3f1c801f2eb875fde
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-03-14 13:51:38 +0600

    find all services data from mongodb

--- index.js
+++ index.js
@@ -38,6 +38,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        //----------------------------Find All Services------------------------------//
+
+        const serviceCollections = client.db('DriveDoctorSolutions').collection('services');
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollections.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
